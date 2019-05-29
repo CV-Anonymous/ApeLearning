960e4ade8bc38ffff3aeea0a4bf0cdd37a3ee702
CV-Anonymous <zhiyuan.chen@horizon.ai>
2019-05-29 17:51:27 +0800

    evaluation criteria and re-ranking

---
+++ Person-ReID/utils/reid_metric.py
@@ -0,0 +1,191 @@
+# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: sherlockliao01@gmail.com
+"""
+
+import numpy as np
+import torch
+from ignite.metrics import Metric
+
+from data.datasets.eval_reid import eval_func
+from .re_ranking import re_ranking
+
+'''
+class R1_mAP(Metric):
+    def __init__(self, num_query, max_rank=50, feat_norm='yes'):
+        super(R1_mAP, self).__init__()
+        self.num_query = num_query
+        self.max_rank = max_rank
+        self.feat_norm = feat_norm
+
+    def reset(self):
+        self.feats = []
+        self.pids = []
+        self.camids = []
+
+    def update(self, output):
+        feat, pid, camid = output
+        self.feats.append(feat)
+        self.pids.extend(np.asarray(pid))
+        self.camids.extend(np.asarray(camid))
+
+    def compute(self):
+        feats = torch.cat(self.feats, dim=0)
+        if self.feat_norm == 'yes':
+            print("The test feature is normalized")
+            feats = torch.nn.functional.normalize(feats, dim=1, p=2)
+        # query
+        qf = feats[:self.num_query]
+        q_pids = np.asarray(self.pids[:self.num_query])
+        q_camids = np.asarray(self.camids[:self.num_query])
+        # gallery
+        gf = feats[self.num_query:]
+        g_pids = np.asarray(self.pids[self.num_query:])
+        g_camids = np.asarray(self.camids[self.num_query:])
+        m, n = qf.shape[0], gf.shape[0]
+        distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
+                  torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+        distmat.addmm_(1, -2, qf, gf.t())
+        distmat = distmat.cpu().numpy()
+        cmc, mAP = eval_func(distmat, q_pids, g_pids, q_camids, g_camids)
+
+        return cmc, mAP
+'''
+
+
+class R1_mAP(Metric):
+    def __init__(self, num_query, max_rank=50, feat_norm='yes'):
+        super(R1_mAP, self).__init__()
+        self.num_query = num_query
+        self.max_rank = max_rank
+        self.feat_norm = feat_norm
+
+    def reset(self):
+        self.feats = []
+        self.pids = []
+        self.camids = []
+
+    def update(self, output):
+        feat, pid, camid = output
+        self.feats.append(feat)
+        self.pids.extend(np.asarray(pid))
+        self.camids.extend(np.asarray(camid))
+
+    def per_eval(self, qf, ql, qc, gf, gl, gc):
+        query = qf.view(-1, 1)
+        score = torch.pow(gf, 2).sum(dim=1) - 2.0 * torch.mm(gf, query).squeeze(1)
+        score = score.cpu()
+        score = score.numpy()
+        index = np.argsort(score)  # from small to large
+
+        # good index
+        query_index = np.argwhere(gl == ql)
+        camera_index = np.argwhere(gc == qc)
+
+        good_index = np.setdiff1d(query_index, camera_index, assume_unique=True)
+        junk_index1 = np.argwhere(gl == -1)
+        junk_index2 = np.intersect1d(query_index, camera_index)
+        junk_index = np.append(junk_index2, junk_index1)  # .flatten())
+
+        CMC_tmp = self.compute_mAP(index, good_index, junk_index)
+        return CMC_tmp
+
+    def compute_mAP(self, index, good_index, junk_index):
+        ap = 0
+        cmc = torch.IntTensor(len(index)).zero_()
+        if good_index.size == 0:  # if empty
+            cmc[0] = -1
+            return ap, cmc
+
+        # remove junk_index
+        mask = np.in1d(index, junk_index, invert=True)
+        index = index[mask]
+
+        # find good_index index
+        ngood = len(good_index)
+        mask = np.in1d(index, good_index)
+        rows_good = np.argwhere(mask == True)
+        rows_good = rows_good.flatten()
+
+        cmc[rows_good[0]:] = 1
+        for i in range(ngood):
+            d_recall = 1.0 / ngood
+            precision = (i + 1) * 1.0 / (rows_good[i] + 1)
+            ap = ap + d_recall * precision
+
+        return ap, cmc
+
+    def compute(self):
+        feats = torch.cat(self.feats, dim=0)
+        if self.feat_norm == 'yes':
+            print("The test feature is normalized")
+            feats = torch.nn.functional.normalize(feats, dim=1, p=2)
+
+        torch.cuda.empty_cache()
+        # query
+        qf = feats[:self.num_query].cuda()
+        q_pids = np.asarray(self.pids[:self.num_query])
+        q_camids = np.asarray(self.camids[:self.num_query])
+        # gallery
+        gf = feats[self.num_query:].cuda()
+        g_pids = np.asarray(self.pids[self.num_query:])
+        g_camids = np.asarray(self.camids[self.num_query:])
+        m, n = qf.shape[0], gf.shape[0]
+
+        CMC = torch.IntTensor(len(g_pids)).zero_()
+        ap = 0.0
+        for i in range(len(q_pids)):
+            ap_tmp, CMC_tmp = self.per_eval(qf[i], q_pids[i], q_camids[i], gf, g_pids, g_camids)
+            if CMC_tmp[0] == -1:
+                continue
+            CMC = CMC + CMC_tmp
+            ap += ap_tmp
+
+        CMC = CMC.float()
+        CMC = CMC / len(q_pids)
+        mAP = ap / len(q_pids)
+        return CMC.numpy(), mAP
+
+class R1_mAP_reranking(Metric):
+    def __init__(self, num_query, max_rank=50, feat_norm='yes'):
+        super(R1_mAP_reranking, self).__init__()
+        self.num_query = num_query
+        self.max_rank = max_rank
+        self.feat_norm = feat_norm
+
+    def reset(self):
+        self.feats = []
+        self.pids = []
+        self.camids = []
+
+    def update(self, output):
+        feat, pid, camid = output
+        self.feats.append(feat)
+        self.pids.extend(np.asarray(pid))
+        self.camids.extend(np.asarray(camid))
+
+    def compute(self):
+        feats = torch.cat(self.feats, dim=0)
+        if self.feat_norm == 'yes':
+            print("The test feature is normalized")
+            feats = torch.nn.functional.normalize(feats, dim=1, p=2)
+
+        # query
+        qf = feats[:self.num_query]
+        q_pids = np.asarray(self.pids[:self.num_query])
+        q_camids = np.asarray(self.camids[:self.num_query])
+        # gallery
+        gf = feats[self.num_query:]
+        g_pids = np.asarray(self.pids[self.num_query:])
+        g_camids = np.asarray(self.camids[self.num_query:])
+        # m, n = qf.shape[0], gf.shape[0]
+        # distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
+        #           torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+        # distmat.addmm_(1, -2, qf, gf.t())
+        # distmat = distmat.cpu().numpy()
+        print("Enter reranking")
+        distmat = re_ranking(qf, gf, k1=20, k2=6, lambda_value=0.3)
+        cmc, mAP = eval_func(distmat, q_pids, g_pids, q_camids, g_camids)
+
+        return cmc, mAP
