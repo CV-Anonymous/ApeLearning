95bd863afa3011e76748867150a468eeca6b40b4
CV-Anonymous <50738139+CV-Anonymous@users.noreply.github.com>
2019-05-29 18:04:05 +0800

    Update Operate_CUHK03.sh

--- Person-ReID/Operate_CUHK03.sh
+++ Person-ReID/Operate_CUHK03.sh
@@ -1,6 +1,6 @@
 # APE Loss for CUHK03-Detected
 python3 tools/train.py --config_file='configs/softmax_triplet_Ours_V1.yml'\
- OUTPUT_DIR "/data/han.sun/Checkpoints/ReID_Strong_BL/cuhk03" \
+ OUTPUT_DIR "/data/Checkpoints/ReID_Strong_BL/cuhk03" \
   LOG_NAME "log_test.txt"\
   OURS.ALPHA "8.0" OURS.BETA "0.5" MODEL.DEVICE_ID "'1'" MODEL.ADJUST_LR "off"\
   MODEL.METRIC_LOSS_TYPE "ours" DATALOADER.SAMPLER "ours" MODEL.NECK "APE"\
