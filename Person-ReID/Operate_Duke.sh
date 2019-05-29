388d89b41402ced577315141b65a75095566463c
CV-Anonymous <zhiyuan.chen@horizon.ai>
2019-05-29 18:12:38 +0800

    Run Duke

--- Person-ReID/Operate_Duke.sh
+++ Person-ReID/Operate_Duke.sh
@@ -1,6 +1,6 @@
 # Our Loss Version 1 for Duke
-python3 tools/train.py --config_file='configs/softmax_triplet_Ours_V1.yml'\
- OUTPUT_DIR "/data/han.sun/Checkpoints/ReID_Strong_BL/Duke" \
+python3 tools/train.py --config_file='configs/APE_Learning.yml'\
+ OUTPUT_DIR "/data/Checkpoints/ReID_Strong_BL/Duke" \
   LOG_NAME "log_test.txt"\
   OURS.ALPHA "18.0" OURS.BETA "0.5" MODEL.DEVICE_ID "'1'" MODEL.ADJUST_LR "off"\
   MODEL.METRIC_LOSS_TYPE "ours" DATALOADER.SAMPLER "ours" MODEL.NECK "APE"\
